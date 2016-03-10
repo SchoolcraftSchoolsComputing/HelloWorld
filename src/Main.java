b616162c0ff661cfc4f998a1034275dc7953ddf6
Mike Nishizawa <michael@fortitude-solutions.com>
2016-03-09 20:15:26 -0500

    step 4

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,15 @@ public class Main {
 
     public static void main(String[] args) {
         for (int i = 0; i < 20; i++) {
-            System.out.println("Hello " + args[0] + "!");
+            System.out.println(pad(i+1, 2)+") Hello " + args[0] + "!");
         }
+    }
+
+    private static String pad(int number, int places) {
+        if(String.valueOf(number).length() < places) {
+            return String.format("%0"+places+"d", number);
+        }
+
+        return String.valueOf(number);
     }
 }
