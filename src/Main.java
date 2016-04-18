73e008d1bc780e3de622a9c4e0b0ce2daa6272f8
Mike Nishizawa <michael@fortitude-solutions.com>
2016-04-17 22:07:06 -0400

    fizzbuzz example

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
+        // iterate through numbers 1-100
        for (int i = 1; i <= 100; i++) {
+           //You must check if it meets both criteria first.
+           //Otherwise it will always print fizz or buzz but
+           // but it will never get to both
            if (i % 3 == 0 && i % 5 == 0) {
                System.out.println("FizzBuzz");
            } else if (i % 3 == 0) {
